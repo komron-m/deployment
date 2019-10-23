cd9f6857cdbf1b2d9b8d1210681d8c958669aa02
Komron <miralizoda.komron@gmail.com>
2019-10-23 13:28:59 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-auto-deployment",
+    version="1.0.0",
+    author="Komron M.",
+    author_email="miralizoda.komron@gmail.com",
+    description="Simple tool for autodeploying github projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/komron-m/simple-auto-deployment",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
