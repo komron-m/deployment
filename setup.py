b34806b9d8008d5b35307666a282836b9ffef66a
komron-m <miralizoda.komron@gmail.com>
2020-12-18 13:34:35 +0500

    Updated setup tools

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deployment",
-    version="1.0.4",
+    version="2.0.0",
     author="Komron Miralizoda",
     author_email="miralizoda.komron@gmail.com",
     description="Simple tool for auto-deployment",
